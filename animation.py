d1197a103ce5c260a39d1acabd19745cf94220de
JenniferWenHsu <jwhsu@berkeley.edu>
2017-01-03 22:04:36 -0800

    ball jumping on simple flat terrain

--- animation.py
+++ animation.py
@@ -45,56 +45,19 @@ class ParticleBox:
         # update positions
         self.state[:, :2] += dt * self.state[:, 2:]
 
-        # # find pairs of particles undergoing a collision
-        # D = squareform(pdist(self.state[:, :2]))
-        # ind1, ind2 = np.where(D < 2 * self.size)
-        # unique = (ind1 < ind2)
-        # ind1 = ind1[unique]
-        # ind2 = ind2[unique]
-
-        # # update velocities of colliding pairs
-        # for i1, i2 in zip(ind1, ind2):
-        #     # mass
-        #     m1 = self.M[i1]
-        #     m2 = self.M[i2]
-
-        #     # location vector
-        #     r1 = self.state[i1, :2]
-        #     r2 = self.state[i2, :2]
-
-        #     # velocity vector
-        #     v1 = self.state[i1, 2:]
-        #     v2 = self.state[i2, 2:]
-
-        #     # relative location & velocity vectors
-        #     r_rel = r1 - r2
-        #     v_rel = v1 - v2
-
-        #     # momentum vector of the center of mass
-        #     v_cm = (m1 * v1 + m2 * v2) / (m1 + m2)
-
-        #     # collisions of spheres reflect v_rel over r_rel
-        #     rr_rel = np.dot(r_rel, r_rel)
-        #     vr_rel = np.dot(v_rel, r_rel)
-        #     v_rel = 2 * r_rel * vr_rel / rr_rel - v_rel
-
-        #     # assign new velocities
-        #     self.state[i1, 2:] = v_cm + v_rel * m2 / (m1 + m2)
-        #     self.state[i2, 2:] = v_cm - v_rel * m1 / (m1 + m2) 
-
         # check for crossing boundary
-        crossed_x1 = (self.state[:, 0] < self.bounds[0] + self.size)
-        crossed_x2 = (self.state[:, 0] > self.bounds[1] - self.size)
+        # crossed_x1 = (self.state[:, 0] < self.bounds[0] + self.size)
+        # crossed_x2 = (self.state[:, 0] > self.bounds[1] - self.size)
         crossed_y1 = (self.state[:, 1] < self.bounds[2] + self.size)
         crossed_y2 = (self.state[:, 1] > self.bounds[3] - self.size)
 
-        self.state[crossed_x1, 0] = self.bounds[0] + self.size
-        self.state[crossed_x2, 0] = self.bounds[1] - self.size
+        # self.state[crossed_x1, 0] = self.bounds[0] + self.size
+        # self.state[crossed_x2, 0] = self.bounds[1] - self.size
 
         self.state[crossed_y1, 1] = self.bounds[2] + self.size
         self.state[crossed_y2, 1] = self.bounds[3] - self.size
 
-        self.state[crossed_x1 | crossed_x2, 2] *= -1
+        # self.state[crossed_x1 | crossed_x2, 2] *= -1
         self.state[crossed_y1 | crossed_y2, 3] *= -1
 
         # add gravity
@@ -103,10 +66,6 @@ class ParticleBox:
 
 #------------------------------------------------------------
 # set up initial state
-# np.random.seed(0)
-# init_state = -0.5 + np.random.random((50, 4))
-# init_state[:, :2] *= 3.9
-
 box = ParticleBox()
 dt = 1. / 30 # 30fps
 
@@ -122,10 +81,8 @@ ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
 particles, = ax.plot([], [], 'bo', ms=6)
 
 # rect is the box edge
-rect = plt.Rectangle(box.bounds[::2],
-                     box.bounds[1] - box.bounds[0],
-                     box.bounds[3] - box.bounds[2],
-                     ec='none', lw=2, fc='none')
+rect = plt.Rectangle((-3.2, -2),
+                     6.4, 0, ec='none', lw=2, fc='none')
 ax.add_patch(rect)
 
 def init():
