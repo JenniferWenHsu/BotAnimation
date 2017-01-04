004d625a3a35043da1277d734c63a155e304aa19
JenniferWenHsu <jwhsu@berkeley.edu>
2017-01-04 05:12:04 -0800

    ball is bouncing along the terrain, but still need fixes

--- classes/environment.py
+++ classes/environment.py
@@ -5,7 +5,7 @@ import matplotlib.animation as animation
 class Environment: 
 	def __init__(self, mBot, mTerrain, 
 				time_elapsed = 0, 
-				bounds = [-10, 10, -10, 10],
+				bounds = [0, 10, -1, 10],
 				size = 0.04,
 				M = 1.0, 
 				G = 9.8): 
@@ -18,27 +18,24 @@ class Environment:
 		self.size = size 
 		self.bounds = bounds
 
-	def isTouch(self):
-		value = self.terrain.getYValue()
-		yPos = self.state[1]
-		return value == yPos
-
 	def timeStep(self, dt): 
 		"""step once by dt seconds"""
 		self.time_elapsed += dt
 		# update positions
 		vx = self.state[2]
 		vy = self.state[3]
-		# print("vx: ", vx)
-		# print("vy: ", vy)
+
 		self.state[0] = self.state[0] + dt*vx
 		self.state[1] = self.state[1] + dt*vy
 
-		# check for touching ground 
-		if self.isTouch: 
-			self.state[2] *= -1
-			self.state[2] *= -1
+		# check for touching ground
+		crossedTerrain = (self.state[1] < 
+			self.terrain.getYValue(self.state[0]) + self.size)
 
+		if crossedTerrain: 
+			self.state[3] *= -1
+			self.state[1] = self.state[1] + self.size
+			
 		crossed_x1 = (self.state[0] < self.bounds[0] + self.size)
 		crossed_x2 = (self.state[0] > self.bounds[1] - self.size)
 		crossed_y1 = (self.state[1] < self.bounds[2] + self.size)
@@ -50,7 +47,7 @@ class Environment:
 		if crossed_y1 | crossed_y2: 
 			self.state[3] *= -1
 		# add gravity
-		# self.state[3] -= self.M * self.G * dt
+		self.state[3] -= self.M * self.G * dt
 
 
 	def startAnimation(self): 
@@ -58,7 +55,7 @@ class Environment:
 		fig = plt.figure()
 		fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 		ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, 
-							xlim = (-10, 10), ylim=(-10, 10))
+							xlim = (0, 10), ylim=(-1, 10))
 		plt.plot(self.terrain.getPointList())
 		particles, = ax.plot([], [], 'bo', ms=6)
 		dt = 1./30
@@ -70,7 +67,7 @@ class Environment:
 		def animate(i): 
 			self.timeStep(dt)
 			particles.set_data([self.state[0]], [self.state[1]])
-			print("state:", (self.state[0], self.state[1]))
+			#print("state:", (self.state[0], self.state[1]))
 			#ms = int(fig.dpi * 2 * self.size * fig.get_figwidth() / np.diff(ax.get_xbound())[0])
 			particles.set_markersize(10)
 			return particles,
--- classes/terrain.py
+++ classes/terrain.py
@@ -30,10 +30,11 @@ class Terrain:
 		plt.show()
 
 	def getYValue(self, x): 
-		x1 = math.floor(x)/self.segLength
-		x2 = math.ceil(x)/self.segLength
-		y1 = self.pointList[localMinPoint]
-		y2 = self.pointList[localMaxPoint]
+		x1 = int(math.floor(x/self.segLength))
+		x2 = int(math.ceil(x/self.segLength))
+		
+		y1 = self.pointList[x1]
+		y2 = self.pointList[x2]
 		m = (float)(y2-y1)/(x1-x2)
 		return y1 + m*(x-x1)
 
