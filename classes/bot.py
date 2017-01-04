f2330adafd7dcf13875298bcd7c0a37e0fa3ab14
JenniferWenHsu <jwhsu@berkeley.edu>
2017-01-04 04:48:27 -0800

    bot is finally moving

--- classes/bot.py
+++ classes/bot.py
@@ -3,7 +3,7 @@ class Bot:
 	init_state is a 4 element tuple, representing (x, y, vx, vy)
     """
 	def __init__(self, 
-				init_state = [-3.2, -2.4, 0.5, 0.7]): 
+				init_state = [5, 5, 0.5, 0]): 
 		self.state = init_state
 
 	def getState(self): 
--- classes/environment.py
+++ classes/environment.py
@@ -5,6 +5,7 @@ import matplotlib.animation as animation
 class Environment: 
 	def __init__(self, mBot, mTerrain, 
 				time_elapsed = 0, 
+				bounds = [-10, 10, -10, 10],
 				size = 0.04,
 				M = 1.0, 
 				G = 9.8): 
@@ -15,6 +16,7 @@ class Environment:
 		self.G = G
 		self.state = self.bot.getState()
 		self.size = size 
+		self.bounds = bounds
 
 	def isTouch(self):
 		value = self.terrain.getYValue()
@@ -27,16 +29,28 @@ class Environment:
 		# update positions
 		vx = self.state[2]
 		vy = self.state[3]
-		self.state[0] = vx*self.state[0]
-		self.state[1] = vy*self.state[1]
+		# print("vx: ", vx)
+		# print("vy: ", vy)
+		self.state[0] = self.state[0] + dt*vx
+		self.state[1] = self.state[1] + dt*vy
 
 		# check for touching ground 
 		if self.isTouch: 
 			self.state[2] *= -1
 			self.state[2] *= -1
 
+		crossed_x1 = (self.state[0] < self.bounds[0] + self.size)
+		crossed_x2 = (self.state[0] > self.bounds[1] - self.size)
+		crossed_y1 = (self.state[1] < self.bounds[2] + self.size)
+		crossed_y2 = (self.state[1] > self.bounds[3] - self.size)
+
+		# bouncing back 
+		if crossed_x1 | crossed_x2: 
+			self.state[2] *= -1
+		if crossed_y1 | crossed_y2: 
+			self.state[3] *= -1
 		# add gravity
-		self.state[3] -= self.M * self.G * dt
+		# self.state[3] -= self.M * self.G * dt
 
 
 	def startAnimation(self): 
@@ -44,7 +58,7 @@ class Environment:
 		fig = plt.figure()
 		fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 		ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, 
-							xlim = (0, 10), ylim=(-1, 7))
+							xlim = (-10, 10), ylim=(-10, 10))
 		plt.plot(self.terrain.getPointList())
 		particles, = ax.plot([], [], 'bo', ms=6)
 		dt = 1./30
@@ -55,9 +69,10 @@ class Environment:
 
 		def animate(i): 
 			self.timeStep(dt)
-			particles.set_data(self.state[0], self.state[1])
-			ms = int(fig.dpi * 2 * self.size * fig.get_figwidth() / np.diff(ax.get_xbound())[0])
-			particles.set_markersize(ms)
+			particles.set_data([self.state[0]], [self.state[1]])
+			print("state:", (self.state[0], self.state[1]))
+			#ms = int(fig.dpi * 2 * self.size * fig.get_figwidth() / np.diff(ax.get_xbound())[0])
+			particles.set_markersize(10)
 			return particles,
 
 		ani = animation.FuncAnimation(fig, animate, frames=600, interval=10, blit=True, init_func=init)
